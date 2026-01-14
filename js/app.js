a4c021c287b68d9adbb7248c051990bdfe2888ae
Nick Konstantinov <nick.konstantinov.job@gmail.com>
2026-01-14 21:53:45 +0400

    feat: add layout and basic logic

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const N = 4;
+let step = 1;
+
+const examplesEl = document.getElementById('examples');
+const button = document.getElementById('submitBtn');
+
+let currentInput = null;
+
+createExample(step);
+
+function createExample(step) {
+  const row = document.createElement('div');
+  row.className = 'example-row';
+
+  const left = document.createElement('div');
+  left.className = 'example-left';
+  left.innerHTML = `${N} × ${step} = `;
+
+  const input = document.createElement('input');
+  input.type = 'number';
+  input.className = 'example__input';
+
+  left.appendChild(input);
+  row.appendChild(left);
+
+  const right = document.createElement('div');
+  right.className = 'example-right';
+  row.appendChild(right);
+
+  examplesEl.appendChild(row);
+  currentInput = input;
+
+  input.addEventListener('input', () => {
+    button.disabled = input.value === '';
+  });
+}
+
+button.addEventListener('click', () => {
+  const answer = Number(currentInput.value);
+  if (answer === N * step) {
+    currentInput.disabled = true;
+    step++;
+    createExample(step);
+  }
+});
